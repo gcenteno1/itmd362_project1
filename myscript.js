cc417d52e299813f4dfc8e90c1ee5eb2fb86a517
giocee <31427610+giocee@users.noreply.github.com>
2019-03-06 17:21:42 -0600

    add function: thankyou()

--- myscript.js
+++ myscript.js
@@ -3,3 +3,6 @@ function checkforblank(){
 		alert('enter email');document.getElementById('email').style.borderColor = "red" ;return false;
 	}
 }
+function thankyou(){
+	alert("Thanks for signing up! Check your email for future deals.");
+}
