67923e37a9a8c0ed62fa074013da7dec15117fb1
giocee <31427610+giocee@users.noreply.github.com>
2019-04-03 17:57:56 -0500

    edit js age validation

--- java/myscript.js
+++ java/myscript.js
@@ -1,8 +1,11 @@
-function checkforblank(){
-	if (document.getElementById('email').value ==''){
-		alert('enter email');document.getElementById('email').style.borderColor = "red" ;return false;
-	}
+function verifyage(){
+	var user_age= document.getElementById.checkOn;
+	if (user_age ==""){
+		alert('Sorry! You need to be at least 18 years old to sign up.');
+		return false;
+	}	
 }
+
 function thankyou(){
 	alert("Thanks for signing up! Check your email for future deals.");
 }
